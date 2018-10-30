9234bb53672251a9f57255aaae14b0df6020a968
Katey Noone <knoone@mica.edu>
2018-10-30 17:12:40 -0400

    cap  & io
    attempting to connect capacitive touch to io

---
+++ CapacitiveTouchAttempt/CapacitiveTouchAttempt.ino
@@ -0,0 +1,41 @@
+#include <CapacitiveSensor.h>
+#include "secrets.h"
+#include <ESP8266WiFi.h>
+int count = 0;
+#define IO_LOOP_DELAY 5000
+unsigned long lastUpdate = 0;
+// set up the 'counter' feed
+
+AdafruitIO_Feed *analog = io.feed("analog");
+
+CapacitiveSensor cs_14_13 = CapacitiveSensor(14, 13);
+
+long sensor_reading;
+
+void setup() {
+  Serial.begin(115200);
+  while(! Serial);
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+}
+
+void loop() {
+  io.run();
+ sensor_reading =  cs_4_6.capacitiveSensor(30);
+
+ Serial.print("sending -> ");
+ Serial.println(sensor_reading);
+ analog->save(sensor_reading);
+ delay(500);
+ 
+  
+}
+

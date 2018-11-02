8cd42a79c4fd6f6c295925e47c57dedb40f96977
Katey Noone <knoone@mica.edu>
2018-11-02 17:30:26 -0400

    cap and led
    // out io stuff since it was making it run weird. Leds and cap will run together but I'm having a hard time getting indevidual pins to create different reactions.

--- MPRled/MPRled.ino
+++ MPRled/MPRled.ino
@@ -12,45 +12,34 @@ uint16_t lasttouched = 0;
 uint16_t currtouched = 0;
 
 //
-////leds
-//#include <FastLED.h>
-//
-//#define LED_PIN     14
-//#define NUM_LEDS    15
-//#define BRIGHTNESS  64
-//#define LED_TYPE    WS2812B
-//#define COLOR_ORDER GRB
-//CRGB leds[NUM_LEDS];
-//       
-//#define UPDATES_PER_SECOND 100
-//CRGBPalette16 currentPalette;
-//TBlendType    currentBlending;
+//leds
+#include <FastLED.h>
+
+#define LED_PIN     14
+#define NUM_LEDS    15
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+       
+#define UPDATES_PER_SECOND 100
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
 
 
 //io
 
- #include "secrets.h"
-
-AdafruitIO_Feed *messages = io.feed("messages");
-
+// #include "secrets.h"
+//
+//AdafruitIO_Feed *messages = io.feed("messages");
+//
 
 void setup() {
   Serial.begin(9600);
 // cap
   while (!Serial) { // needed to keep leonardo/micro from starting too fast!
     delay(10);
-     Serial.print("connecting to adafruit IO");
- 
-  io.connect();
-  messages->onMessage(handleMessage);
-  while(io.status() < AIO_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
 
- 
-  Serial.println();
-  Serial.println(io.statusText());
     
     
   }
@@ -61,33 +50,44 @@ void setup() {
     while (1);
   }
   Serial.println("MPR121 found!");
-//
-// //led
-//    delay( 3000 ); // power-up safety delay
-//    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-//    FastLED.setBrightness(  BRIGHTNESS );
-//    pinMode(14, OUTPUT);
+
+ //led
+    delay( 3000 ); // power-up safety delay
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+   // pinMode(14, OUTPUT);
+   
+    currentPalette = RainbowColors_p; // palette the led's automatically start on
+    currentBlending = LINEARBLEND;
 
 //io stuff
  //  while(! Serial);
-   Serial.print("connecting to adafruit IO");
- 
-  io.connect();
-  messages->onMessage(handleMessage);
-  while(io.status() < AIO_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
- 
-  Serial.println();
-  Serial.println(io.statusText());
+//   Serial.print("connecting to adafruit IO");
+// 
+//  io.connect();
+//  messages->onMessage(handleMessage);
+//  while(io.status() < AIO_CONNECTED) {
+//    Serial.print(".");
+//    delay(500);
+//  }
+//
+// 
+//  Serial.println();
+//  Serial.println(io.statusText());
 
 }
 
 void loop() {
-  io.run();
-  
+//  io.run();
+   //led
+    static uint8_t startIndex = 0;
+    startIndex = startIndex +1; //motion speed
+
+   FastLED.setBrightness(  BRIGHTNESS );
+    FillLEDsFromPaletteColors( startIndex);
+    FastLED.show();
+    FastLED.delay(1000 / UPDATES_PER_SECOND);
+    
   // Get the currently touched pads
   currtouched = cap.touched();
   
@@ -95,7 +95,6 @@ void loop() {
     // it if *is* touched and *wasnt* touched before, alert!
     if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
       Serial.print(i); Serial.println(" touched");
-      //messages->save("yes" + i);
     }
     // if it *was* touched and now *isnt*, alert!
     if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
@@ -107,37 +106,29 @@ void loop() {
   lasttouched = currtouched;
 
 
-//  
-//    //led
-//    static uint8_t startIndex = 0;
-//    startIndex = startIndex +1; //motion speed
-//
-//   FastLED.setBrightness(  BRIGHTNESS );
-//    FillLEDsFromPaletteColors( startIndex);
-//    FastLED.show();
-//    FastLED.delay(1000 / UPDATES_PER_SECOND);
+ 
 }
 
 
 
 //led
-//void FillLEDsFromPaletteColors( uint8_t colorIndex)
-//{
-//  
-//    uint8_t brightness = 255;
-//    
-//    for( int i = 0; i < NUM_LEDS; i++) {
-//        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-//        colorIndex += 3;
-//    }
-//}
-//
-//
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+  
+    uint8_t brightness = 255;
+    
+    for( int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
+
+
 
 //io
-void handleMessage(AdafruitIO_Data *data) {
+//void handleMessage(AdafruitIO_Data *data) {
+//
+//  Serial.print("received <- ");
+//  Serial.println(data->value());
 
-  Serial.print("received <- ");
-  Serial.println(data->value());
-
-}
+//}
